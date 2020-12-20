431d2b73901036a15a54cde49cb110f31132ca01
Huemac Alfredo Córdova <hmca101292@outlook.com>
2020-12-19 21:59:21 -0600

    Create rspec test case for pick_lines method

---
+++ spec/file_reading_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/file_reading.rb'
+
+describe Opening do
+
+  describe '#pick_lines' do
+    let(:v) { Opening.new('./css/sample_good.css') }
+    it 'Opens a file, read it and returns a string array with the lines of the file' do
+      expect(v.pick_lines[1, 2]).to eql(["  margin: 0;", "  padding: 0;"])
+    end
+  end
+end 
